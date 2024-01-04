8ee1e77e2a28f6f3bc3385f68b9ac7536e8f3a53
MackorLab <76862637+MackorLab@users.noreply.github.com>
2024-01-04 06:30:37 +0300

    Update musicgen_app.py

--- demos/musicgen_app.py
+++ demos/musicgen_app.py
@@ -340,41 +340,7 @@ def ui_full(launch_kwargs):
             """
             ### More details
 
-            The model will generate a short music extract based on the description you provided.
-            The model can generate up to 30 seconds of audio in one pass.
 
-            The model was trained with description from a stock music catalog, descriptions that will work best
-            should include some level of details on the instruments present, along with some intended use case
-            (e.g. adding "perfect for a commercial" can somehow help).
-
-            Using one of the `melody` model (e.g. `musicgen-melody-*`), you can optionally provide a reference audio
-            from which a broad melody will be extracted.
-            The model will then try to follow both the description and melody provided.
-            For best results, the melody should be 30 seconds long (I know, the samples we provide are not...)
-
-            It is now possible to extend the generation by feeding back the end of the previous chunk of audio.
-            This can take a long time, and the model might lose consistency. The model might also
-            decide at arbitrary positions that the song ends.
-
-            **WARNING:** Choosing long durations will take a long time to generate (2min might take ~10min).
-            An overlap of 12 seconds is kept with the previously generated chunk, and 18 "new" seconds
-            are generated each time.
-
-            We present 10 model variations:
-            1. facebook/musicgen-melody -- a music generation model capable of generating music condition
-                on text and melody inputs. **Note**, you can also use text only.
-            2. facebook/musicgen-small -- a 300M transformer decoder conditioned on text only.
-            3. facebook/musicgen-medium -- a 1.5B transformer decoder conditioned on text only.
-            4. facebook/musicgen-large -- a 3.3B transformer decoder conditioned on text only.
-            5. facebook/musicgen-melody-large -- a 3.3B transformer decoder conditioned on and melody.
-            6. facebook/musicgen-stereo-*: same as the previous models but fine tuned to output stereo audio.
-
-            We also present two way of decoding the audio tokens
-            1. Use the default GAN based compression model. It can suffer from artifacts especially
-                for crashes, snares etc.
-            2. Use [MultiBand Diffusion](https://arxiv.org/abs/2308.02560). Should improve the audio quality,
-                at an extra computational cost. When this is selected, we provide both the GAN based decoded
-                audio, and the one obtained with MBD.
 
             See [github.com/facebookresearch/audiocraft](https://github.com/facebookresearch/audiocraft/blob/main/docs/MUSICGEN.md)
             for more details.
